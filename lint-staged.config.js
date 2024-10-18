b75e7578cc8cf067ae4034f8b8e74eed54d148ad
Gonzalo D'Elia <gonzalo@bloq.com>
2024-10-18 11:21:59 -0300

    Improve patter for lint-staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,6 +2,6 @@ const formatFiles = "prettier --ignore-unknown --write";
 const sortPackageJson = "better-sort-package-json";
 
 export default {
-  "socials.json|*.{js,md,ts,yml}": [formatFiles],
+  "!(package.json)": [formatFiles],
   "package.json": [sortPackageJson, formatFiles],
 };
